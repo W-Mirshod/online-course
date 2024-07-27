7575d328b540171c2f75618ddd8d7b4f4485f60a
Mirshod <python3526@gmail.com>
2024-07-28 04:42:18 +0500

    apps ar created

--- root/settings.py
+++ root/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'course.apps.CourseConfig'
+    'courses.apps.CoursesConfig',
+    'teachers.apps.TeachersConfig',
 ]
 
 MIDDLEWARE = [
