aa987ab8728c3ff6023a4ca8c18a50f6b2733a3c
Mirshod <python3526@gmail.com>
2024-07-30 12:49:40 +0500

    major improvement completed

--- root/settings.py
+++ root/settings.py
@@ -100,8 +100,11 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'courses/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'courses/static/')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
